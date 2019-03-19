c0897fb5b7003fa6601d8dd9ed2a55bd917a7c2a
rohan484909 <rai807949@gmail.com>
2019-03-19 15:44:40 +0530

    create new book or delete book

--- about controller/LiraryManagementSystem/app/controllers/book_controller.rb
+++ about controller/LiraryManagementSystem/app/controllers/book_controller.rb
@@ -2,4 +2,28 @@ class BookController < ApplicationController
   def index
   	@books = Book.all
   end
+  def new
+  	@book = Book.new 
+  end
+  def create
+  @book = Book.create(book_params)
+  
+  	redirect_to '/'
+  end
+
+  
+  def show
+  	
+ end
+
+  def destroy
+  @book = Book.find(params[:id])
+     @book.destroy
+
+     redirect_to "/"
+  end
+  def book_params
+  	params.require(:book).permit(:title, :content)
+  end
+
 end
--- about controller/LiraryManagementSystem/config/routes.rb
+++ about controller/LiraryManagementSystem/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :book
   root 'book#index'
   get 'book/:id/show' => "book#show"
+  get 'book/new' => "book#new" 
+  post 'book/new' => 'book#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
