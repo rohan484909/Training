1b48ff3c0a5737afaeb2db6d982978dcbcf08c5b
rohan484909 <rai807949@gmail.com>
2019-03-26 11:02:20 +0530

    used sever side valiadtion and callbacks

--- LibrarayManmnagement2/app/models/student.rb
+++ LibrarayManmnagement2/app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ApplicationRecord
 	belongs_to :teacher, optional: true
 	has_many :books
+	validates  :name, presence: true
+	before_validation :ensure_login_has_a_value
+	def ensure_login_has_a_value
+		if name.nil?
+			puts "name cant blank"
+		end
+	end
 end
--- LibrarayManmnagement2/db/schema.rb
+++ LibrarayManmnagement2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_134635) do
+ActiveRecord::Schema.define(version: 2019_03_26_044818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2019_03_25_134635) do
   create_table "teachers", force: :cascade do |t|
     t.string "name"
     t.string "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
