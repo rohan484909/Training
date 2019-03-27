5e1ba5e9b71bb39a3e909d301d429ad1dbea5db6
rohan484909 <rai807949@gmail.com>
2019-03-27 16:44:24 +0530

    pracice of tag

--- AttendanceManagementSystem/app/controllers/registration_controller.rb
+++ AttendanceManagementSystem/app/controllers/registration_controller.rb
@@ -1,17 +1,25 @@
 class RegistrationController < ApplicationController
-	 before_action :set_employee, only: [:show, :edit, :update, :destroy]
+	
   def index
   	@employees = Employee.all
   end
 
   def new
   	@employee = Employee.new
+
   end
 
   def edit
   end
 
   def show
+    
+    @pet_name = params[:pet_dog]
+    @age_child = params[:age_child]
+    puts @pet_name
+    puts @age_child
+    
+
   end
   def set_employee
   	@employee = Employee.find(params[:id])
--- AttendanceManagementSystem/config/routes.rb
+++ AttendanceManagementSystem/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :employees
   resources :registration
   root 'registration#index'
+  get  '/index' => "registration#show"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
