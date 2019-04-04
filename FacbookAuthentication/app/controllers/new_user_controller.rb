a66d3f4e363e97af70410e27e058482b7de65258
rohan484909 <rai807949@gmail.com>
2019-04-04 15:32:26 +0530

    adding config file and created curd operation

--- FacbookAuthentication/app/controllers/new_user_controller.rb
+++ FacbookAuthentication/app/controllers/new_user_controller.rb
@@ -7,12 +7,22 @@ class NewUserController < ApplicationController
     @user = User.new
   end
   def create
-  	
+    @user = User.create(user_params)
+  		if @user.save
+  			UserMailer.welcome_email(@user).deliver_now
+   	   	render :show
+      else
+        render :new 
+      end
   end
 
   def edit
   end
 
   def show
+  end
+
+  def user_params
+   	params.require(:user).permit(:name, :email, :password)
   end
 end
--- FacbookAuthentication/app/mailers/user_mailer.rb
+++ FacbookAuthentication/app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
-	default from: 'notifications@example.com'
-	def welcome_email
-    @user = params[:user]
+	default from: 'rai807949@gmail.com'
+	
+	def welcome_email(user)
+    @user = user
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
