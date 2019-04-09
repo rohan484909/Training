a78d1233094581b4ca3d1bf1bb1c0ed0e930eb2d
rohan484909 <rai807949@gmail.com>
2019-04-09 12:00:24 +0530

    permit image column in product_params

--- E_commerce/app/controllers/products_controller.rb
+++ E_commerce/app/controllers/products_controller.rb
@@ -72,6 +72,6 @@ class ProductsController < ApplicationController
     end
     
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name, :price, :image)
     end
 end
