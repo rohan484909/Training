bb72eb29c0ddca3fc70aa28c8719cf8c78a5f522
rohan484909 <rai807949@gmail.com>
2019-04-09 12:02:42 +0530

    add attachements method and validation

--- E_commerce/app/models/product.rb
+++ E_commerce/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+	#validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
 end
