98989f665e4398bf04a9816c181499cfe7960d09
rohan484909 <rai807949@gmail.com>
2019-04-10 19:14:35 +0530

    added routes for devise

--- E_commerce/config/routes.rb
+++ E_commerce/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+	root 'products#index'
+	devise_for :users
 	get 'products/search', to: 'products#search', as: 'search'
-  resources :products
+       resources :products
+       
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
