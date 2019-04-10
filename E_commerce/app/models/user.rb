ffededfb0fb82c80dc9f4623b8d0380a6e8fcb41
rohan484909 <rai807949@gmail.com>
2019-04-10 15:52:54 +0530

    added attachement file and validation

---
+++ E_commerce/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+	#validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
+end
