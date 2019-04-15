50ffd798396354bbd4e245c9a8c5ce8eb7d6c893
rohan484909 <rai807949@gmail.com>
2019-04-15 13:59:37 +0530

    adding google authentiation method

--- FacbookAuthentication/app/controllers/users/omniauth_callbacks_controller.rb
+++ FacbookAuthentication/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-   
+   protect_from_forgery  with: :null_session
   def facebook
 
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -19,6 +19,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         set_flash_message(:notice, :success, :kind => "github") if is_navigational_format?
       else
         session["devise.github_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+  end
+
+  def google_oauth2
+    @user = User.from_omniauth_google(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication
+        set_flash_message(:notice, :success, :kind => "google") if is_navigational_format?
+      else 
+        session["devise.google_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
       end
   end
