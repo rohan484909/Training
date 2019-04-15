ed1f5ab1903d681e6e972a8bc72ee3d3cc980511
rohan484909 <rai807949@gmail.com>
2019-04-15 14:02:15 +0530

    adding new method and also add google auth provider

--- FacbookAuthentication/app/models/user.rb
+++ FacbookAuthentication/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :validatable, :omniauthable, 
-         :omniauth_providers => [:facebook, :github]
+         :omniauth_providers => [:facebook, :github, :google_oauth2]
   mount_uploader :avatar, ProfileUploader
 
   def self.new_with_session(params, session)
@@ -19,11 +19,19 @@ class User < ApplicationRecord
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      debugger
-      user.email = "rai807949@gmail.com"#  auth.info.email
+      user.email = "rai807949@gmail.com"       #auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      user.token = auth.token
+    end
+  end
+  def self.from_omniauth_google(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.token = auth.credentials.token
+      user.expires = auth.credentials.expires
+      user.expires_at = auth.credentials.expires_at
+      user.refresh_token = auth.credentials.refresh_token
     end
   end
 end
