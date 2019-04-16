ab47b20b7232c8dc2fc02bc145c45a6d6c99e5bf
rohan484909 <rai807949@gmail.com>
2019-04-16 11:59:13 +0530

    it is used for client side validation

---
+++ ponies/app/assets/javascripts/validation.js
@@ -0,0 +1,18 @@
+/*jQuery(function($) {
+ $("#signup").validate({
+    rules: {
+      "#employee_name": {
+       required: true
+       },
+      "#employee_profession": {
+      required:true
+      },
+    },
+  });
+});*/
+$(document).ready(function() {
+  $('#sbt').click(function(event){
+    alert('You clicked on the form');
+    
+  });
+});
