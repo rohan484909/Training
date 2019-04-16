745fe542292c8215eb6dbc1502e40ddee0c83987
rohan484909 <rai807949@gmail.com>
2019-04-16 12:00:52 +0530

    adding method to check valid user

--- ponies/app/models/employee.rb
+++ ponies/app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+	validate :check_employee_name
+
+	def check_employee_name
+    source = File.read(Rails.root.join("app/assets/javascripts/newemployee.js"))
+	  context = ExecJS.compile(source)
+	  unless context.call("isValidName", name)
+	  	errors.add :name, "is an invalid number"
+	  end
+	end
 end
