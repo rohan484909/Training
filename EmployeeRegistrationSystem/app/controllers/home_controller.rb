b865b4fbb40dad4672a5e48ffc2539df51eccb5a
rohan484909 <rai807949@gmail.com>
2019-04-18 19:37:46 +0530

    create next1 method and session object also used in

--- EmployeeRegistrationSystem/app/controllers/home_controller.rb
+++ EmployeeRegistrationSystem/app/controllers/home_controller.rb
@@ -3,5 +3,19 @@ class HomeController < ApplicationController
   end
 
   def new
+  	@employee = Employee.new
+  end
+
+  def next1
+  	debugger
+  	session[:name] = params[:name]
+    session[:email] = params[:email]
+    session[:phone] = params[:phone]
+    session[:address] = params[:address]
+    @employee = Employee.new({name: session[:name],email: session[:email],phone: session[:phone],address: session[:address]}) if session[:address]
+    if session[:address]
+    	@employee.save
+    end
   end
 end
+ 
