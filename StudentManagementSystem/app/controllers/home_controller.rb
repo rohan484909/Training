c26b8bde3ab4629b497ec43fdec53c1f1a9480ea
rohan484909 <rai807949@gmail.com>
2019-04-18 12:07:25 +0530

    create new method change

--- StudentManagementSystem/app/controllers/home_controller.rb
+++ StudentManagementSystem/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def change
+  debugger
+  	@name = params[:name]
+  	@phone = params[:phone]
+  	@email = params[:email]
+  	@address = params[:address]
+  end
 end
