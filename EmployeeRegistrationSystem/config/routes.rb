bf0b624ecc8041a5b6d10f823a8349026c184f11
rohan484909 <rai807949@gmail.com>
2019-04-19 15:37:16 +0530

    setting routes for index page

--- EmployeeRegistrationSystem/config/routes.rb
+++ EmployeeRegistrationSystem/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/new'
   post '/next1' => "home#next1"
+  post '/home/index' => "home#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
