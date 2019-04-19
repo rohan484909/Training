494afd1bc73c60b6246413823c0f954261d7ab1f
rohan484909 <rai807949@gmail.com>
2019-04-19 18:10:46 +0530

    added routes for feedback post request

--- EmployeeRegistrationSystem/config/routes.rb
+++ EmployeeRegistrationSystem/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'feedback#index'
+  get '/feedback/new'
   get 'home/index'
   get 'home/new'
   post '/next1' => "home#next1"
   post '/home/index' => "home#create"
+  post '/feedback/new' => "feedback#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
