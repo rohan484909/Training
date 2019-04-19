96fb29e0ab38325ad1ed42163a0513fe5ca0eb4c
rohan484909 <rai807949@gmail.com>
2019-04-19 16:30:35 +0530

    valiadte email and other text field through javascript

---
+++ EmployeeRegistrationSystem/app/assets/javascripts/validation.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+	$('#contact_name').on('input', function() {
+		var input=$(this);
+		alert(input.val());
+		var is_name=input.val();
+			if(is_name){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+
+$(document).ready(function(){	
+	$('#contact_email').on('input', function() {
+		var input=$(this);
+		var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		var is_email=re.test(input.val());
+			if(is_email){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+$(document).ready(function(){
+	$('#contact_phone').on('input', function() {
+		var input=$(this);
+		alert(input.val());
+		var is_name=input.val();
+			if(is_name){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+$(document).ready(function(){
+	$('#contact_address').on('input', function() {
+		var input=$(this);
+		alert(input.val());
+		var is_name=input.val();
+			if(is_name){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+$(document).ready(function(){
+	$('#contact_salary').on('input', function() {
+		var input=$(this);
+		alert(input.val());
+		var is_name=input.val();
+			if(is_name){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+$(document).ready(function(){
+	$('#contact_department').on('input', function() {
+		var input=$(this);
+		alert(input.val());
+		var is_name=input.val();
+			if(is_name){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+	});
+});
+
+$(document).ready(function(){
+ $("#contact_submit").click(function(event){
+	var form_data=$("#contact").serializeArray();
+
+	var error_free=true;
+	for (var input in form_data){
+		
+		var element = $("#contact_"+form_data[input]['name']);
+		alert(form_data[input]['name']);
+
+		var valid = element.hasClass("valid");
+		var error_element = $("span", element.parent());
+		if (!valid){error_element.removeClass("error").addClass("error_show"); error_free=false;}
+		else{error_element.removeClass("error_show").addClass("error");}
+	}
+	if (!error_free){
+		event.preventDefault(); 
+	}
+	else{
+		alert('No errors: Form will be submitted');
+	}
+});
+});
+
+// $(document).ready(function(){
+//   $("#clickme").click(function(){
+//     alert("The paragraph was clicked.");
+//   });
+// });
+
